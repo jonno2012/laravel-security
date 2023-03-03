26edaa92d5e834d292b2a5fb2d25e705bda2e024
jonno2012 <jonathanrhill78@gmail.com>
2023-03-03 14:15:52 +0000

    Markdown security concerns

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\HtmlString;
 use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Support\Str;
+
 class Book extends Model
 {
     use HasFactory;
@@ -13,5 +15,15 @@ class Book extends Model
     public function escapedDescription(): Attribute
     {
         return Attribute::get(fn () => new HtmlString(nl2br(e($this->description))));
+    }
+
+    public function escapedMarkdownDescription(): Attribute
+    {
+        return Attribute::get(fn () => Str::of($this->description)->markdown([
+            'html_input' => 'escape',
+            'allow_unsafe_links' => false,
+            'max_nesting_level' => 5,
+        ])->toHtmlString()
+        );
     }
 }

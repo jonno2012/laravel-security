1f1c6c028b9747930ad40e3e1240d27950b5a016
jonno2012 <jonathanrhill78@gmail.com>
2023-03-03 08:26:22 +0000

    SQL Injection, SQL Map and the Apostrophe trick

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Http\Request;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,6 +16,16 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/sql_example', function(Request $request) {
+    $books = \App\Models\Book::query()
+        ->when($request->get('search'), fn($q, $s) => $q->whereRaw("slug LIKE ?", ["%{$s}%"]))
+        ->whereNotNull('published_at')
+        ->oldest()
+        ->get(); // paramaterised queries
+
+    return view('welcome', ['books' => $books]);
 });
 
 Route::get('/dashboard', function () {

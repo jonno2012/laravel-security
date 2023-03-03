52b283f3c237cbc472d3702c9812ed2f454afe5a
jonno2012 <jonathanrhill78@gmail.com>
2023-03-03 13:39:01 +0000

    Never trust user input

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Support\HtmlString;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 class Book extends Model
 {
     use HasFactory;
+
+    public function escapedDescription(): Attribute
+    {
+        return Attribute::get(fn () => new HtmlString(nl2br(e($this->description))));
+    }
 }
